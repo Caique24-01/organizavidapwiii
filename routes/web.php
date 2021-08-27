555a2b4182d766a82cb0df7d640974c086a11f37
Caique24-01 <caiqueleme100@gmail.com>
2021-08-27 20:25:58 -0300

    cria rota mytasks retornando view welcome

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/mytasks', function(){
+}); 
+Route::get('/mytasks', function () {
     return view('welcome');
-})
+});
