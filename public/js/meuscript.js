50a6e03c43cca43bd3875a9823c1a499ac33763e
Caique24-01 <caiqueleme100@gmail.com>
2021-09-23 22:17:21 -0300

    Teste API

--- public/js/meuscript.js
+++ public/js/meuscript.js
@@ -4,5 +4,9 @@ var __webpack_exports__ = {};
   !*** ./resources/js/meuscript.js ***!
   \***********************************/
 alert("meu script");
+var url = 'https://swapi.dev/api/pessoas/';
+fetch(url + '1/').then(function (response) {
+  console.log(response);
+});
 /******/ })()
 ;
