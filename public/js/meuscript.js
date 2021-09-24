f425accf4bc84c079ae7e293fbf7246f91161d54
Caique24-01 <caiqueleme100@gmail.com>
2021-09-24 19:44:44 -0300

    Teste API

--- public/js/meuscript.js
+++ public/js/meuscript.js
@@ -3,7 +3,7 @@ var __webpack_exports__ = {};
 /*!***********************************!*\
   !*** ./resources/js/meuscript.js ***!
   \***********************************/
-alert("meu script");
+alert("Organiza Vida");
 var url = 'https://swapi.dev/api/people/';
 fetch(url + '2').then(function (response) {
   return response.json();
